9f3c1c37eb9ae79035559990a2e8fda63ad2bf4c
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-11-20 18:01:18 +0100

    [dashboard][xs]: temp add ts-expect-error
    - https://github.com/vercel/next.js/issues/42292

--- app/app/(dashboard)/page.tsx
+++ app/app/(dashboard)/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import Sites from "@/components/sites";
 /* import OverviewStats from "@/components/overview-stats"; */
-import Posts from "@/components/posts";
 /* import Link from "next/link"; */
 import PlaceholderCard from "@/components/placeholder-card";
 import OverviewSitesCTA from "@/components/overview-sites-cta";
@@ -9,19 +8,13 @@ import OverviewSitesCTA from "@/components/overview-sites-cta";
 export default function Overview() {
     return (
         <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
-            {/* <div className="flex flex-col space-y-6">
-        <h1 className="font-cal text-3xl font-bold dark:text-white">
-          Overview
-        </h1>
-        <OverviewStats />
-      </div> */}
-
             <div className="flex flex-col space-y-6">
                 <div className="flex items-center justify-between">
                     <h1 className="font-cal text-3xl font-bold dark:text-white">
                         Top Sites
                     </h1>
                     <Suspense fallback={null}>
+                        {/* @ts-expect-error Server Component */}
                         <OverviewSitesCTA />
                     </Suspense>
                 </div>
@@ -34,6 +27,7 @@ export default function Overview() {
                         </div>
                     }
                 >
+                    {/* @ts-expect-error Server Component */}
                     <Sites limit={4} />
                 </Suspense>
             </div>
