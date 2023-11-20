bcfb4d67b3ce1e0d3c27dcf8d2830aeb442fa9f5
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-11-20 17:55:59 +0100

    [providers][xs]: rm sessionprovider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,27 @@
 import { headers } from "next/headers";
 
 import { TRPCReactProvider } from "@/trpc/react";
-import { SessionProvider } from "next-auth/react";
 import { Toaster } from "sonner";
 import { ModalProvider } from "@/components/modal/provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <SessionProvider>
-            <TRPCReactProvider headers={headers()}>
-                <Toaster className="dark:hidden" />
-                <Toaster theme="dark" className="hidden dark:block" />
-                <ModalProvider>{children}</ModalProvider>
-            </TRPCReactProvider>
-        </SessionProvider>
+        <TRPCReactProvider headers={headers()}>
+            <Toaster className="dark:hidden" />
+            <Toaster theme="dark" className="hidden dark:block" />
+            <ModalProvider>{children}</ModalProvider>
+        </TRPCReactProvider>
     );
 }
+
+/* import { SessionProvider } from "next-auth/react"; */
+
+/* export function Providers({ children }: { children: React.ReactNode }) {
+*     return (
+*         <SessionProvider>
+*                 <Toaster className="dark:hidden" />
+*                 <Toaster theme="dark" className="hidden dark:block" />
+*                 <ModalProvider>{children}</ModalProvider>
+*         </SessionProvider>
+*     );
+* } */
